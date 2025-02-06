ec273f25229520092deda821e381057235a3505e
Shivam Prajapati <100841132+itsgeekhokage@users.noreply.github.com>
2025-02-06 18:18:45 +0530

    Update mongo.js

--- server/db/mongo.js
+++ server/db/mongo.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI = 'mongodb://127.0.0.1:27017/trainingcrm';
+const MONGO_URI = process.env.MONGO_URI;
 
 const connectDB = async () => {
     try {
